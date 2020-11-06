827fd0e20b2d74b4ffd3132c62918d891cfb0b31
Akhil Ghatiki <akhil.ghatiki@gmail.com>
2020-11-06 12:30:44 +0530

    Implement configurable options for pipeline

--- src/main/java/com/google/swarm/sqlserver/migration/FileImportPipeline.java
+++ src/main/java/com/google/swarm/sqlserver/migration/FileImportPipeline.java
@@ -1,10 +1,20 @@
 package com.google.swarm.sqlserver.migration;
 
+import static com.google.swarm.sqlserver.migration.common.pipelineConfiguration.Constants.DATA_SET;
+import static com.google.swarm.sqlserver.migration.common.pipelineConfiguration.Constants.DLP_CONFIG_BUCKET;
+import static com.google.swarm.sqlserver.migration.common.pipelineConfiguration.Constants.DLP_CONFIG_OBJECT;
+import static com.google.swarm.sqlserver.migration.common.pipelineConfiguration.Constants.FILE_DE_LIMITER;
+import static com.google.swarm.sqlserver.migration.common.pipelineConfiguration.Constants.INPUT_FILE_PATH;
+import static com.google.swarm.sqlserver.migration.common.pipelineConfiguration.Constants.PROJECT;
+import static com.google.swarm.sqlserver.migration.common.pipelineConfiguration.Constants.TEMP_LOCATION;
+
 import com.google.api.services.bigquery.model.TableRow;
 import com.google.swarm.sqlserver.migration.common.fileImport.FileImportPipelineOptions;
 import com.google.swarm.sqlserver.migration.common.fileImport.FileRowToBQRowConverter;
 import com.google.swarm.sqlserver.migration.common.fileImport.FileTableSchema;
 import com.google.swarm.sqlserver.migration.common.fileImport.config.TableSchemaConfigUtil;
+import com.google.swarm.sqlserver.migration.common.pipelineConfiguration.PipelineRunnerConfigUtil;
+import java.util.Map;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
@@ -19,30 +29,36 @@ public class FileImportPipeline {
 
   public static final Logger LOG = LoggerFactory.getLogger(FileImportPipeline.class);
 
-  public static void runFileImportPipeline(String[] args) {
+  public void runFileImportPipeline(String[] args) {
 
     FileImportPipelineOptions fileImportPipelineOptions = PipelineOptionsFactory.fromArgs(args)
         .withValidation().as(FileImportPipelineOptions.class);
-
-    runFileImport(fileImportPipelineOptions);
+    System.out.println("------->>>" + INPUT_FILE_PATH);
+    runFileImport(enrichOptions(fileImportPipelineOptions));
 
   }
 
-//  public static FileImportPipelineOptions generateOptions() {
-//    FileImportPipelineOptions fileImportPipelineOptions = PipelineOptionsFactory
-//        .as(FileImportPipelineOptions.class);
-//
-//    PipelineRunnerConfigUtil pipelineRunnerConfigUtil = new PipelineRunnerConfigUtil();
-//
-//    Map<String, String> fileImportOptionsMap = pipelineRunnerConfigUtil.getPipelineConfigMap()
-//        .getFileImportPipelineConfig().getOptions().getPipelineOptionsMap();
-//
-//    fileImportPipelineOptions
-//        .setProject(fileImportOptionsMap.get(PROJECT));
-//    fileImportPipelineOptions.setDataSet(DATA_SET);
-//  }
+  private FileImportPipelineOptions enrichOptions(
+      FileImportPipelineOptions fileImportPipelineOptions) {
 
-  public static void runFileImport(FileImportPipelineOptions options) {
+    PipelineRunnerConfigUtil pipelineRunnerConfigUtil = new PipelineRunnerConfigUtil();
+
+    Map<String, String> fileImportOptionsMap = pipelineRunnerConfigUtil.getPipelineConfigMap()
+        .getPipelineConfig().getFileImportPipelineConfig().getOptions();
+
+    fileImportPipelineOptions
+        .setProject(fileImportOptionsMap.get(PROJECT));
+    fileImportPipelineOptions.setDataSet(fileImportOptionsMap.get(DATA_SET));
+    fileImportPipelineOptions.setInputFilePath(fileImportOptionsMap.get(INPUT_FILE_PATH));
+    fileImportPipelineOptions.setFileDeLimiter(fileImportOptionsMap.get(FILE_DE_LIMITER));
+    fileImportPipelineOptions.setTempLocation(fileImportOptionsMap.get(TEMP_LOCATION));
+    fileImportPipelineOptions.setDLPConfigBucket(fileImportOptionsMap.get(DLP_CONFIG_BUCKET));
+    fileImportPipelineOptions.setDLPConfigObject(fileImportOptionsMap.get(DLP_CONFIG_OBJECT));
+
+    return fileImportPipelineOptions;
+  }
+
+  public void runFileImport(FileImportPipelineOptions options) {
 
     Pipeline importPipeline = Pipeline.create(options);
 
